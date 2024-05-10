d549284dc49c0c6050b0403566f209cb3eb47b20
kreker2112 <kreker2112@gmail.com>
2024-05-10 13:13:45 +0300

    install dependencies

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,18 @@ const generalVarsFiles = ['/assets/scss/_variables.scss'];
 
 export default defineConfig({
     transpileDependencies: true,
+    publicPath:
+        process.env.NODE_ENV === 'production' ? '/converter.github.io' : '/',
+    chainWebpack: (config) => {
+        config.plugin('define').tap((definitions) => {
+            Object.assign(definitions[0], {
+                __VUE_OPTIONS_API__: JSON.stringify(true),
+                __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+            });
+            return definitions;
+        });
+    },
     css: {
         loaderOptions: {
             scss: {
